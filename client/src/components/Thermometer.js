b7ee065315a3fa36a97a5a83886fceb259340375
Emery Valencia <emeryvalencia@gmail.com>
2018-11-08 21:03:24 -0800

    Added other components, not yet implemented

--- client/src/components/Thermometer.js
+++ client/src/components/Thermometer.js
@@ -2,5 +2,24 @@
   /* <div class="progress">
 	<!--TODO: set style.background-color based on the audio feature's color and width via the audio feature's percent. Display feature name and percent.-->
   <div class="progress-bar" role="progressbar">???: ???</div>
-</div> */
+</div> 
+
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thermometer',
+  templateUrl: './thermometer.component.html',
+  styleUrls: ['./thermometer.component.css']
+})
+export class ThermometerComponent implements OnInit {
+  //TODO: define Input fields and bind them to the template.
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+*/
 }
