752aec20e1ceecaa34d87e3d8932d32e8171742a
juarezeppy <jejuarez88@gmail.com>
2018-11-09 00:01:02 -0800

    added html for albums and similar artist

--- client/src/components/ArtistPage.js
+++ client/src/components/ArtistPage.js
@@ -4,7 +4,7 @@ import defaultImage from '../assets/unknown.jpeg';
 import './ArtistPage.css';
 
 class ArtistPage extends React.Component {
-  state = { displayName: '', imageURL: '', externalURL: '', genres: [] };
+  state = { displayName: '', imageURL: '', externalURL: '', genres: [], tracks: [] };
   componentDidMount() {
     // do fetch with id
     // then update state
@@ -33,11 +33,21 @@ class ArtistPage extends React.Component {
     fetch(`http://localhost:8888/artist-top-tracks/${this.props.match.params.id}`).then(
       (response) => {
         const body = response.json().then((parsedBody) => {
-          console.log('sartist-top-tracks parsedBody :', parsedBody);
+          console.log('artist-top-tracks parsedBody :', parsedBody);
+          const trackCopy = parsedBody.tracks.concat();
+          this.setState({
+            ...this.state,
+            tracks: trackCopy
+          });
         });
       }
     );
   }
+  msToMinSecConversion = (duration_ms) => {
+    var min = Math.floor(duration_ms / 60000);
+    var sec = ((duration_ms % 60000) / 1000).toFixed(0);
+    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+  };
 
   render() {
     console.log('ArtistPage this.props', this.props);
@@ -46,6 +56,7 @@ class ArtistPage extends React.Component {
     const { displayName } = this.state;
     const { externalURL } = this.state;
     const { genres } = this.state;
+    const { tracks } = this.state;
 
     let genresElements = null;
     genresElements = genres.map((genre) => {
@@ -55,92 +66,113 @@ class ArtistPage extends React.Component {
     //creates an array of tracks and track information
     let trackArray = [];
 
+    console.log('tracks:', tracks.length);
+
     //REFACTOR THIS
-    // for (let i = 0; i < 20; i++) {
-    //   //initialize a tempArray to hold each track's info
-    //   let tempArray = [];
+    for (let i = 0; i < tracks.length; i++) {
+      //initialize a tempArray to hold each track's info
+      let tempArray = [];
 
-    //   //get information from each track
-    //   const { name } = this.parsedBody.tracks.items[i];
-    //   const { duration_ms } = this.parsedBody.tracks.items[i];
-    //   const duration = this.msToMinSecConversion(duration_ms);
-    //   const artist = this.parsedBody.tracks.items[i].artists[0].name;
-    //   const artistLink = this.parsedBody.tracks.items[i].artists[0].external_urls.spotify;
+      //get information from each track
+      const { name } = tracks[i];
+      const { duration_ms } = tracks[i];
+      const duration = this.msToMinSecConversion(duration_ms);
+      const artist = tracks[i].artists[0].name;
+      const artistLink = tracks[i].artists[0].external_urls.spotify;
 
-    //   //links
-    //   const album = this.parsedBody.tracks.items[i].album.name;
-    //   const albumLink = this.parsedBody.tracks.items[i].album.external_urls.spotify;
-    //   const trackLink = this.parsedBody.tracks.items[i].external_urls.spotify;
+      //links
+      const album = tracks[i].album.name;
+      const albumLink = tracks[i].album.external_urls.spotify;
+      const trackLink = tracks[i].external_urls.spotify;
 
-    //   //push contents of track into the tempArray
-    //   tempArray.push(i + 1); //pushes the number of the track/search number
-    //   tempArray.push(name);
-    //   tempArray.push(duration);
-    //   tempArray.push(artist);
-    //   tempArray.push(album);
+      //push contents of track into the tempArray
+      tempArray.push(i + 1); //pushes the number of the track/search number
+      tempArray.push(name);
+      tempArray.push(duration);
+      tempArray.push(artist);
+      tempArray.push(album);
 
-    //   //links necessary for redirection
-    //   tempArray.push(trackLink); //at track[5]
-    //   tempArray.push(artistLink); //at track[6]
-    //   tempArray.push(albumLink); //at track[7]
+      //links necessary for redirection
+      tempArray.push(trackLink); //at track[5]
+      tempArray.push(artistLink); //at track[6]
+      tempArray.push(albumLink); //at track[7]
 
-    //   //add this tracks info to the trackArray
-    //   trackArray.push(tempArray);
-    // }
+      //add this tracks info to the trackArray
+      trackArray.push(tempArray);
+    }
+
+    console.log('trackArray', trackArray);
 
     //contains the elements to be rendered on the homepage
     let elements = (
-      <div className="row">
-        {/*Artist Info*/}
-        <div className="col-6">
-          <h3>{displayName} </h3>
-          <img id="artistImg" src={this.state.imageURL} alt="" />
-          <p>
-            <a href={externalURL} className="btn btn-light" target="_blank">
-              Open {displayName} on Spotify
-            </a>
-          </p>
+      <Fragment>
+        <div className="row">
+          {/*Artist Info*/}
+          <div className="col-6">
+            <h3>{displayName} </h3>
+            <img id="artistImg" src={this.state.imageURL} alt="" />
+            <p>
+              <a href={externalURL} className="btn btn-light" target="_blank">
+                Open {displayName} on Spotify
+              </a>
+            </p>
+          </div>
+          {/*Genres and tracks*/}
+          <div className="col-2">
+            <h3>Genres</h3>
+            <ul>{genresElements}</ul>
+            <p />
+          </div>
+          <div className="col-4">
+            <h3>{displayName}'s Top Tracks</h3>
+            <table className="table table-sm table-light table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Track</th>
+                  <th scope="col">Duration</th>
+                  <th scope="col">Artist</th>
+                  <th scope="col"> Album</th>
+                </tr>
+              </thead>
+              <tbody>
+                {trackArray.map((track, index) => {
+                  //updates table on main search page
+                  return (
+                    <tr key={index}>
+                      <td>{track[0]}</td>
+                      <td>
+                        <a href={track[5]} target="_blank">
+                          {track[1]}
+                        </a>
+                      </td>
+                      <td>{track[2]}</td>
+                      <td>
+                        <a href={track[6]} target="_blank">
+                          {track[3]}
+                        </a>
+                      </td>
+                      <td>
+                        <a href={track[7]} target="_blank">
+                          {track[4]}
+                        </a>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
-        {/*Genres and tracks*/}
-        <div className="col-2">
-          <h3>Genres</h3>
-          <ul>{genresElements}</ul>
-          <p />
+        <div className="row">
+          <div className="col-6">
+            <h3>{displayName}'s Albums</h3>
+          </div>
+          <div className="col-6">
+            <h3>Similar Artists</h3>
+          </div>
         </div>
-        <div className="col-4">
-          <h3>{displayName}'s Top Tracks</h3>
-        </div>
-        <div>
-          {/* <table className="table table-sm table-light table-striped">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Track</th>
-                <th scope="col">Duration</th>
-                {this.expandedTable()}
-              </tr>
-            </thead>
-            <tbody>
-              {trackArray.map((track, index) => {
-                //updates table on main search page
-                return (
-                  <tr key={index}>
-                    {/*not sure what to put for key for now
-                    <td>{track[0]}</td>
-                    <td>
-                      <a href={track[5]} target="_blank">
-                        {track[1]}
-                      </a>
-                    </td>
-                    <td>{track[2]}</td>
-                    {this.searchTrackElement(track)}
-										</tr>
-                );
-              })}
-            </tbody>
-          </table> */}
-        </div>
-      </div>
+      </Fragment>
     );
 
     return <div className="app-artist">{elements}</div>;
